1063644fdcf6acb1c34f7bcc0b40d27ae7d14db7
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-02-19 19:52:00 -0500

    Fixing indentation of index.html

--- js/action-form.js
+++ js/action-form.js
@@ -1,28 +1,27 @@
-const daysChecked = document.querySelectorAll(".checkbox");
+const daysChecked = document.querySelectorAll('.checkbox');
 // const submitButton = document.querySelector("button");
-const forms = document.querySelector("form");
+const forms = document.querySelector('form');
 
-let daysPicked =[]; 
-forms.addEventListener("submit",function (e){
-    e.preventDefault();
-    const courseTitle = forms.elements.courseTitle.value;
-    const color = forms.elements.color.value;
-    const time = forms.elements.time.value;
-    const location = forms.elements.location.value;
-    const professor = forms.elements.professor.value;
-    console.log(courseTitle);
-    console.log(color);
+let daysPicked = [];
+forms.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const courseTitle = forms.elements.courseTitle.value;
+  const color = forms.elements.color.value;
+  const time = forms.elements.time.value;
+  const location = forms.elements.location.value;
+  const professor = forms.elements.professor.value;
+  console.log(courseTitle);
+  console.log(color);
 
-    daysChecked.forEach(checkbox =>{
-        if(checkbox.checked){
-            daysPicked.push(true);
-        }
-        else{
-            daysPicked.push(false);
-        }
-    });
-    console.log(daysPicked);
-    console.log(time);
-    console.log(location);
-    console.log(professor);
+  daysChecked.forEach((checkbox) => {
+    if (checkbox.checked) {
+      daysPicked.push(true);
+    } else {
+      daysPicked.push(false);
+    }
+  });
+  console.log(daysPicked);
+  console.log(time);
+  console.log(location);
+  console.log(professor);
 });
